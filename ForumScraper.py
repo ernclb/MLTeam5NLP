2dbc5f3f60c4c9794d2c08148a289dba848f13c7
ErnclbMentor <ernclb@mentorchains.com>
2020-06-07 19:06:20 -0400

    Updated the Script to write the dictionary into the text file

--- ForumScraper.py
+++ ForumScraper.py
@@ -3,6 +3,8 @@ import pprint
 import json
 page_val = str(input('What page would you like to print: '))
 url = "https://community.bnz.co.nz/latest.json?no_definitions=true&page="+page_val
-r = requests.get(url)
-dic = json.loads(r.text)
+r = (requests.get(url)).text
+dic = json.loads(r)
+with open('text.txt', 'w') as f:
+    print(r, file=f)
 pprint.pprint(dic)
