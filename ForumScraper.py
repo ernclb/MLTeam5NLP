780c75cfd270a410ab7c4dd8b98b591bd19309d7
ErnclbMentor <ernclb@mentorchains.com>
2020-06-08 11:24:28 -0400

    started scraping didn't define certain functions to scrape new zealand bank website

--- ForumScraper.py
+++ ForumScraper.py
@@ -1,10 +1,28 @@
 import requests
 import pprint
 import json
-page_val = str(input('What page would you like to print: '))
-url = "https://community.bnz.co.nz/latest.json?no_definitions=true&page="+page_val
-r = (requests.get(url)).text
-dic = json.loads(r)
-with open('text.txt', 'w') as f:
-    print(r, file=f)
-pprint.pprint(dic)
+
+page_amount = 7
+i = 1
+new_submissions = []
+while len(dic['topic_list']['topics']) > 0:
+    url = "https://community.bnz.co.nz/latest.json?no_definitions=true&page="+i
+    r = (requests.get(url)).text
+    raw_dictionary = json.loads(r)
+    clean_dictionary = cleans_dictionary(raw_dictionary)
+    new_submissions.append(clean_dictionary)
+    i += 1
+
+topic_url = "https://community.bnz.co.nz/t/"
+for submission in new_submissions:
+    topic_url += (submission['slug'] + '/' + submission['topic_id'])
+    message_in_html = requests.get(topic_url).text
+    get_forum_message(message_in_html)
+
+def get_forum_message(message):
+    #Use the html to parse and get the message submission
+    
+def cleans_dictionary(raw_dictionary):
+    #get all the slugs and topic_id's out of this dictionary (make sure the parameters are string)
+    #and make a new dictionary with the 30 topics just the id's and slugs
+    #new_submissions = [{'topic_id': 23124,'slug':'debit-cards'},{'topic_id': 846283,'slug':'credit-card'}]
