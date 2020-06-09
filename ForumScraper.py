21dc61407fedcf8c99923f94fa89bd5a710f1f91
badhri123 <bs190@snu.edu.in>
2020-06-08 23:28:47 -0400

    Reordered position of functions. Commented out incomplete function for checking the output of cleans_dictionary. Obtaining the output new_submissions.

--- ForumScraper.py
+++ ForumScraper.py
@@ -2,34 +2,10 @@ import requests
 import pprint
 import json
 
-page_amount = 7
+# page_amount = 7
 i = 1
 new_submissions = []
 
-
-while 1:
-    clean_dictionary_list=[]
-    url = "https://community.bnz.co.nz/latest.json?no_definitions=true&page="+str(i)
-    r = (requests.get(url)).text
-    raw_dictionary = json.loads(r)
-    raw_dictionary_topics = raw_dictionary["topic_list"]["topics"]
-    if len(raw_dictionary_topics['topic_list']['topics']) == 0:
-        break
-      
-    clean_dictionary_list = cleans_dictionary(raw_dictionary_topics)
-    new_submissions += clean_dictionary_list
-
-    i += 1
-
-topic_url = "https://community.bnz.co.nz/t/"
-for submission in new_submissions:
-    topic_url += (submission['slug'] + '/' + submission['topic_id'])
-    message_in_html = requests.get(topic_url).text
-    get_forum_message(message_in_html)
-
-def get_forum_message(message):
-    #Use the html to parse and get the message submission
-    
 def cleans_dictionary(raw_dictionary):
     #get all the slugs and topic_id's out of this dictionary (make sure the parameters are string)
     #and make a new dictionary with the 30 topics just the id's and slugs
@@ -38,8 +14,50 @@ def cleans_dictionary(raw_dictionary):
     
     for dic in raw_dictionary:
         temp_dict=dict()
-        temp_dict['topic_id'] = dic['topic_id']
+        temp_dict['topic_id'] = dic['id']
         temp_dict['slug'] = dic['slug']
         list_dictionary.append(temp_dict)
-    return list_dictionary    
+
+
+    return list_dictionary  
+
+
+# Commented out the following for checking output
+# def get_forum_message(message):
+#     #Use the html to parse and get the message submission  
+
+
+      
+
+
+while 1:
+
+    clean_dictionary_list=[]
+    url = "https://community.bnz.co.nz/latest.json?no_definitions=true&page="+str(i)
+    r = (requests.get(url)).text
+    raw_dictionary = json.loads(r)
+    raw_dictionary_topics = raw_dictionary["topic_list"]["topics"]
+    if len(raw_dictionary_topics) == 0:
+        break
+      
+    clean_dictionary_list = cleans_dictionary(raw_dictionary_topics)
+    new_submissions += clean_dictionary_list
+
+    i += 1
+
+
+print("New Submissions\n")
+print(new_submissions)   
+
+# Commented the following to check the output of the program
+
+# topic_url = "https://community.bnz.co.nz/t/"
+# for submission in new_submissions:
+#     topic_url += (submission['slug'] + '/' + submission['topic_id'])
+#     message_in_html = requests.get(topic_url).text
+#     get_forum_message(message_in_html)
+
+
+    
+  
 
