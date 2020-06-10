27054f98b48d74dcbf4682596ced9f2866271bac
saiparsaSTEM <66392611+saiparsaSTEM@users.noreply.github.com>
2020-06-10 16:48:09 -0400

    few changes in structure

--- ForumScraper.py
+++ ForumScraper.py
@@ -19,36 +19,47 @@ def cleans_dictionary(raw_dictionary):
 def get_forum_message(page):    
 
     soup = BeautifulSoup(page.content, 'html.parser')
-    for anchors in soup.find_all('a'): #To remove hyperlinks
+    for anchors in soup.find_all('a'):
       anchors.extract()
 
     postData = soup.find_all("p")
     
     posts = []
     for post in postData:
-        posts.append(BeautifulSoup(str(post)).get_text().strip().replace("\t", ""))#removing tabs
+        posts.append(BeautifulSoup(str(post)).get_text().strip().replace("\r",""))
 
-    posts_stripped = [x.replace("\n","") for x in posts]#removing newlines
+    posts_stripped = [x.replace("\n","") for x in posts]
 
     return posts_stripped
+  
 
-page_amount = 7
+#page_amount = 1
 new_submissions = []
 clean_posts=[]
-for page in range(1,page_amount+1) :
-    url = "https://community.bnz.co.nz/latest.json?no_definitions=true&page="+str(page)
+#for page in range(1,page_amount+1) :
+page=0
+while True:
+    page=page+1
+    url = "https://choice.community/latest.json?no_definitions=true&page=1"+str(page)
     r = (requests.get(url)).text
     raw_dictionary = json.loads(r)
+    if len(raw_dictionary['topic_list']['topics']) <= 0:
+      print('Done')
+      break
+    if (page%5==0):
+      print("Page",page)
     cleans_dictionary(raw_dictionary)
     
+with open("Choice_Community_slug.pickle", "wb") as output_file:
+  pickle.dump(new_submissions, output_file)
 
-topic_url = "https://community.bnz.co.nz/t/"
+topic_url = "https://choice.community/t/"
+print("Total Topics:",len(new_submissions))
 for submission in new_submissions:
     topic_url += (submission['slug'] + '/' + submission['id'])
     page = requests.get(topic_url)
     clean_posts.append([topic_url,get_forum_message(page)])
-    topic_url = "https://community.bnz.co.nz/t/"
+    topic_url = "https://choice.community/t/"
 
-
-with open("Data.pickle", "wb") as output_file:
+with open("Choice_Community_text.pickle", "wb") as output_file:
     pickle.dump(clean_posts, output_file)
